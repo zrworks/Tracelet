2adf9b021dc164019d5428f2d3f641d9f1ec8697
KiranBennyJoseph <kiranbjm@gmail.com>
2026-05-26 10:25:06 +0530

    fix(ios): add missing FlutterFramework dependency to SPM package

--- packages/tracelet_ios/ios/tracelet_ios/Package.swift
+++ packages/tracelet_ios/ios/tracelet_ios/Package.swift
@@ -15,12 +15,14 @@ let package = Package(
         // Consumed via Swift Package Manager when imported as a Flutter plugin.
         // It points to the root of the Tracelet monorepo where the SPM configuration resides.
         .package(url: "https://github.com/Ikolvi/Tracelet.git", revision: "sdk-ios-v3.0.0"),
+        .package(name: "FlutterFramework", path: "../FlutterFramework"),
     ],
     targets: [
         .target(
             name: "tracelet_ios",
             dependencies: [
                 .product(name: "TraceletSDK", package: "Tracelet"),
+                .product(name: "FlutterFramework", package: "FlutterFramework"),
             ],
             path: "Sources/tracelet_ios",
             resources: [

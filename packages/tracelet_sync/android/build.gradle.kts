1d9fe9e34d02de174b46afc14e021461404aa3be
KiranBennyJoseph <kiranbjm@gmail.com>
2026-06-10 09:29:41 +0530

    chore: update TraceletSDK version to 3.2.11 and align dependencies in podspecs and changelogs

--- packages/tracelet_sync/android/build.gradle.kts
+++ packages/tracelet_sync/android/build.gradle.kts
@@ -73,7 +73,7 @@ kotlin {
 
 dependencies {
     compileOnly("com.ikolvi:tracelet-sdk:3.2.11")
-    implementation("com.ikolvi:tracelet-sync-sdk:3.2.10")
+    implementation("com.ikolvi:tracelet-sync-sdk:3.2.11")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.mockito:mockito-core:5.0.0")
 }

93b18ea15a8e4b1b6d1cdb33de8610bd99a6349d
KiranBennyJoseph <kiranbjm@gmail.com>
2026-06-13 12:22:38 +0530

    chore: release 3.2.19

--- packages/tracelet_sync/android/build.gradle.kts
+++ packages/tracelet_sync/android/build.gradle.kts
@@ -72,8 +72,8 @@ kotlin {
 }
 
 dependencies {
-    compileOnly("com.ikolvi:tracelet-sdk:3.2.18")
-    implementation("com.ikolvi:tracelet-sync-sdk:3.2.18")
+    compileOnly("com.ikolvi:tracelet-sdk:3.2.19")
+    implementation("com.ikolvi:tracelet-sync-sdk:3.2.19")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.mockito:mockito-core:5.0.0")
 }
